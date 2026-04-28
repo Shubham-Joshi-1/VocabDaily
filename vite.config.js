f15be2319f9ec482008e14755b533967b020c840
Shubham Joshi <shubhamjoshi2321@gmail.com>
2026-04-29 02:33:41 +0530

    fix base path for gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vocab-daily-web/',
+  base: '/VocabDaily/',
   server: {
     port: 3000,
   },
